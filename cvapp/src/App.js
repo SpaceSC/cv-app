8a278200f0d64f3c132ab038640f2874840e792d
SpaceSC <ceciliasandor.22@gmail.com>
2021-02-11 10:44:23 +0100

    New component with fetch

--- cvapp/src/App.js
+++ cvapp/src/App.js
@@ -3,15 +3,18 @@ import TextDisplay from './components/TextDisplay/TextDisplay.component.jsx';
 import TextArea from './components/TextArea/TextArea.component.jsx';
 import ResetButton from './components/ResetButton/ResetButton.component.jsx';
 import './App.css';
+import LeftFields from './components/TextArea/TextArea.component.jsx';
 
-function App() {
+function App () {
+
 
   const [displayText, setDisplayText] = useState("");
 
   return (
     <div className="App">
       <ResetButton reset={() => setDisplayText("")}/>
-      <div class="container">
+      <div className="container">
+        <LeftFields/>
         <TextArea display={displayText} changeText={(event) => setDisplayText(event.target.value)}/>
         <TextDisplay display={displayText}/>
       </div>
--- cvapp/src/components/TextArea/TextArea.component.jsx
+++ cvapp/src/components/TextArea/TextArea.component.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./TextArea.component.css";
 
+/*
 function TextArea(props) {
   return (
     <textarea id="text-area" name="text-area" value={props.display} onChange={props.changeText}></textarea>
   );
 }
 export default TextArea;
+*/
+
+export function LeftFields () {
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    let data = fetch('../../cv.json')
+      .then(response => response.json())
+      .then((result) => { setData(result) });
+      }, [setData]);
+
+      console.log(data);
+      
+  return (
+    <div className="cvData">
+      {data}
+    </div>
+
+    )
+}
+
+export default LeftFields;
